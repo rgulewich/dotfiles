368ecb3ef1d7d314bff6d4e67269f667ffde47c4
Rob Gulewich <rgulewich@netflix.com>
2021-01-02 09:56:27 -0800

    Load local nix config if it exists

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,9 +1,15 @@
 { pkgs, ... }:
-{
+
+let
+  home_dir = builtins.getEnv "HOME";
+  local_imports = if builtins.pathExists("${home_dir}/etc/local.nix") then ["${home_dir}/etc/local.nix"] else [];
+
+in
+rec {
   imports = [
     ./alacritty.nix
     ./tmux.nix
     ./vim.nix
     ./zsh.nix
-  ];
+  ] ++ local_imports;
 }
