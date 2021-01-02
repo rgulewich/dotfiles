c50c9d07c98edee7ce22eba3397ed66cef4cff87
Rob Gulewich <rgulewich@netflix.com>
2021-01-01 17:43:26 -0800

    nix: install zsh dir

--- nixpkgs/zsh.nix
+++ nixpkgs/zsh.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 {
   home.file.".zshrc".source = "${config.home.homeDirectory}/src/me/dotfiles/zshrc";
+  home.file.".zsh".source = "${config.home.homeDirectory}/src/me/dotfiles/zsh";
   home.file.".profile.d/common".source = "${config.home.homeDirectory}/src/me/dotfiles/profile.d/common";
   home.file.".profile.d/git".source = "${config.home.homeDirectory}/src/me/dotfiles/profile.d/git";
   home.file.".profile.d/local".source = "${config.home.homeDirectory}/etc/profile.d/local";
