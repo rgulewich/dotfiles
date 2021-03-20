6257bf000eacaeb292b594261f59b0dd608fe4c8
Rob Gulewich <rgulewich@netflix.com>
2021-03-20 15:55:15 -0700

    Add some osx settings

--- nixpkgs/osx-settings.nix
+++ nixpkgs/osx-settings.nix
@@ -1,5 +1,10 @@
 { config, pkgs, ... }:
 {
+
+  # see https://daiderd.com/nix-darwin/manual/index.html#sec-options
   system.defaults.dock.autohide = true;
   system.defaults.finder.FXEnableExtensionChangeWarning = false;
+  system.defaults.finder.AppleShowAllExtensions = true;
+
+  system.defaults.trackpad.Clicking = true;
 }
