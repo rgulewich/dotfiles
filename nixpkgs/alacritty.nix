2d72c3717172faa2c474a79b0fab5a274d549c1a
Rob Gulewich <rgulewich@netflix.com>
2024-07-04 09:35:47 -0700

    Move to auto-migrated alacritty toml file

--- nixpkgs/alacritty.nix
+++ nixpkgs/alacritty.nix
@@ -4,5 +4,5 @@
   # config reference:
   #   https://github.com/alacritty/alacritty/blob/master/alacritty.yml
   #
-  home.file.".config/alacritty/alacritty.yml".source = "${config.home.homeDirectory}/src/me/dotfiles/config/alacritty/alacritty.yml";
+  home.file.".config/alacritty/alacritty.toml".source = "${config.home.homeDirectory}/src/me/dotfiles/config/alacritty/alacritty.toml";
 }
